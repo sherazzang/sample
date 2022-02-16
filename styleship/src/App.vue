55bf6e013a9f8619f021472072f53c5ccbcc9a80
sherazzang <shera31@naver.com>
2022-02-16 17:59:08 +0900

    first

---
+++ styleship/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div> -->
+  <div>
+    <nav>
+      <ul>
+        <li><a href="">COMPANY</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+
+// #nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
+/* HelveticaNeueLTStd-Roman */
+@font-face {
+    font-family: 'HelveticaNeueLTStd-Roman';
+    src: url('./assets/fonts/HelveticaNeueLTStd-Roman.eot');
+    src: url('./assets/fonts/HelveticaNeueLTStd-Roman.eot?#iefix') format('embedded-opentype'),
+         url('./assets/fonts/HelveticaNeueLTStd-Roman.woff') format('woff'),
+         url('./assets/fonts/HelveticaNeueLTStd-Roman.ttf') format('truetype'),
+         url('./assets/fonts/HelveticaNeueLTStd-Roman.svg#HelveticaNeueLTStd-Roman') format('svg');
+    font-weight: normal;
+    font-style: normal;
+}
+
+/* HelveticaNeueLTStd-Md */
+@font-face {
+    font-family: 'HelveticaNeueLTStd-Md';
+    src: url('./assets/fonts/HelveticaNeueLTStd-Md.eot');
+    src: url('./assets/fonts/HelveticaNeueLTStd-Md.eot?#iefix') format('embedded-opentype'),
+         url('./assets/fonts/HelveticaNeueLTStd-Md.woff') format('woff'),
+         url('./assets/fonts/HelveticaNeueLTStd-Md.ttf') format('truetype'),
+         url('./assets/fonts/HelveticaNeueLTStd-Md.svg#HelveticaNeueLTStd-Md') format('svg');
+    font-weight: normal;
+    font-style: normal;
+}
+
+/* HelveticaNeueLTStd-Bd */
+@font-face {
+    font-family: 'HelveticaNeueLTStd-Bd';
+    src: url('./assets/fonts/HelveticaNeueLTStd-Bd.eot');
+    src: url('./assets/fonts/HelveticaNeueLTStd-Bd.eot?#iefix') format('embedded-opentype'),
+         url('./assets/fonts/HelveticaNeueLTStd-Bd.woff') format('woff'),
+         url('./assets/fonts/HelveticaNeueLTStd-Bd.ttf') format('truetype'),
+         url('./assets/fonts/HelveticaNeueLTStd-Bd.svg#HelveticaNeueLTStd-Bd') format('svg');
+    font-weight: normal;
+    font-style: normal;
+}
+
+</style>
